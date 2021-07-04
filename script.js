84074bc3b0ade30e55117cb6a4cd6130a9f8fc97
Caitlin <86273653+17023@users.noreply.github.com>
2021-07-04 23:28:35 +0000

    Completed popup

---
+++ script.js
@@ -0,0 +1,7 @@
+function buttonPopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+setTimeout(function(){
+  document.getElementById('popup').style.display = "block";
+}, 3500)
