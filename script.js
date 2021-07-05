bf8ce96dc0cb7ee33352a039e8c530091771b454
Caitlin <86273653+17023@users.noreply.github.com>
2021-07-05 08:23:35 +0000

    Help Page.

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-function buttonPopup() {
+function closePopup() {
   document.getElementById("popup").style.display = "none";
 }
 
 setTimeout(function(){
   document.getElementById('popup').style.display = "block";
 }, 3500)
+
+function buttonPopup() { //
+  var x = document.getElementById("examplePopup");
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", "");}, 2500)
+}
