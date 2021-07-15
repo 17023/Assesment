95a5b0cd05c92d79831687dcd02d6e5f726e50a1
Caitlin <86273653+17023@users.noreply.github.com>
2021-07-15 23:21:52 +0000

    Started trying to use an image-map.

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+//Popup
+setTimeout(function(){
+  document.getElementById('popup').style.display = "block";
+}, 3500);
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+//How to use button
+function buttonPopup() { //
+  var x = document.getElementById("examplePopup");
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", "");}, 2500);
+}
