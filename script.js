31ec3716f25adccfff55506b6481bea1faab13ab
Caitlin <86273653+17023@users.noreply.github.com>
2021-09-10 05:34:24 +0000

    Finished all pages, however started contact page to add javascript

--- script.js
+++ script.js
@@ -1,33 +1,17 @@
-//Popup at the beginning of the website
-setTimeout(function(){
-  document.getElementById('popup').style.display = "block";
-}, 2700);
-
-//Closes popup at the beginning of the website
-function closePopup() {
-  document.getElementById("popup").style.display = "none";
-}
-
-//How to use button
-function buttonPopup() {
-  var x = document.getElementById("examplePopup");
-  x.className = "show";
-  setTimeout(function(){ x.className = x.className.replace("show", "");}, 2500);
-}
-
+//Javascript at the bottom of HTML files as well.
 //Hides beginning text when area(s) on image map clicked
 function beginningText() {
   document.getElementById("beginningText").style.display = "none";
 }
 
-//Displays accordion id image map failed
+//Displays accordion if image map failed
 function imageMapFailed() {
-  document.getElementById("failedInfo").style.display = "block";
-  document.getElementById("imageMap").style.display = "none";
+  document.getElementById("failedInfo").style.display = "block"; //changes "failedInfo" to block when button clicked
+  document.getElementById("imageMap").style.display = "none"; //Hides "imageMap" when button clicked
 }
 
 //Reloads the image map if user wants to go back
 function goBack() {
-  document.getElementById("imageMap").style.display = "block";
-  document.getElementById("failedInfo").style.display = "none";
+  document.getElementById("imageMap").style.display = "block"; //changes "imageMap" to block when button clicked
+  document.getElementById("failedInfo").style.display = "none"; //Hides "failedInfo" when button clicked
 }
